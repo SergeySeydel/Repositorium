a3fd9972c128e7258cc7a8c8307ba73af3ba7398
SergeySeydel <60809017+SergeySeydel@users.noreply.github.com>
2020-03-31 14:56:22 +0300

    Update Second.cpp

--- Desktop/Repositorium/Second.cpp
+++ Desktop/Repositorium/Second.cpp
@@ -8,6 +8,7 @@ int main(int argc, char **argv)
 
     const int ARR_SZ = 5;
     const int ARR_SZ2 = 5;
+    const int val = 5;
     int StaticOneArray[ARR_SZ];
     int StaticDoubleTwoArray[ARR_SZ][ARR_SZ2];
 
@@ -17,23 +18,23 @@ int main(int argc, char **argv)
 		DynamicTwoArray[i] = new int[ARR_SZ];
 
 	cout << "Статический одноразмерный массив:" << endl;
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < val; ++i)
 	{
 		cout <<&(StaticOneArray[i]) << " ";
 	}
 	cout << endl;
 
 	cout << "Динамический :" << endl;
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < val; ++i)
 	{
 		cout <<&(DynamicArray[i]) << " ";
 	}
 	cout << endl;
 
 	cout << "Статический двуразмерный массив:" << endl;
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < val; ++i)
 	{
-		for (int j = 0; j < 5; ++j)
+		for (int j = 0; j < val; ++j)
 		{
 			cout <<&(StaticDoubleTwoArray[i][j]) << " ";
 		}
@@ -41,16 +42,16 @@ int main(int argc, char **argv)
 	}
 
 	cout << "Динамический двуразмерный массив:" << endl;
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < val; ++i)
 	{
-		for (int j = 0; j < 5; ++j)
+		for (int j = 0; j < val; ++j)
 		{
 			cout <<&(DynamicTwoArray[i][j]) << " ";
 		}
 		cout << endl;
 	}
 
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < val; ++i)
 	delete[] DynamicTwoArray[i];
 	delete[] DynamicTwoArray;
 	delete[] DynamicArray;
